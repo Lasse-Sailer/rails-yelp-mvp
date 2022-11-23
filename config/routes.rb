3f1d83b7009edcd72878ff1aabd0e49ae4bf82a7
Lasse Sailer <l.sailer@online.de>
2022-11-23 13:01:30 +0100

    added model for review and restaurant, added controller and routes for restaurants (needs revision)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resource :restaurants
+  # resource :restaurants do
+  #   resource :reviews, only: [:new]
+  # end
 end
